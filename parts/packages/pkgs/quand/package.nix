9fe1d6a8a066ad29a7628ec52c8a5982fc5d757a
Ratakor <ratakor@disroot.org>
2025-08-30 21:14:07 +0200

    Add quand

---
+++ parts/packages/pkgs/quand/package.nix
@@ -0,0 +1,35 @@
+# requires coreutils and util-linux but I cba make a wrapper for them
+# also find a way to use dash instead of bash for interpreting
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+}: let
+  rev = "5894842960a09e2d3b35eba3900c548a781001e5";
+in
+  stdenv.mkDerivation (finalAttrs: {
+    pname = "quand";
+    version = "0.4-${rev}";
+
+    outputs = ["out" "man"];
+
+    src = fetchFromGitHub {
+      owner = "ratakor";
+      repo = "quand";
+      inherit rev;
+      hash = "sha256-PbBadaunDCaey52C2ZVtdM84mC1iDXKGmSQmDqVe0zQ=";
+    };
+
+    preBuild = ''
+      makeFlagsArray+=(
+        "PREFIX=$out"
+      )
+    '';
+
+    meta = {
+      description = "Simple calendar written in POSIX sh";
+      homepage = "https://github.com/ratakor/quand";
+      license = lib.licenses.gpl3Plus;
+      mainProgram = "quand";
+    };
+  })
--- users/ratakor/programs/terminal/calendar/default.nix
+++ users/ratakor/programs/terminal/calendar/default.nix
@@ -1,4 +1,37 @@
-# TODO: aur/quand-git # a calendar app like when
-{pkgs, ...}: {
-  xdg.configFile."quand/config".source = ./config;
+# A calendar app like `when`
+# TODO: Rewrite quand in rust & integrate icstocal
+{
+  lib,
+  self,
+  ...
+}: let
+  inherit (builtins) isInt isString typeOf;
+
+  mkValueString = v:
+    if isInt v
+    then toString v
+    else if isString v
+    then "\"${v}\""
+    else if true == v
+    then "1"
+    else if false == v
+    then "0"
+    else throw "Unsupported value type: ${typeOf v}";
+
+  toKeyValue = lib.generators.toKeyValue {
+    mkKeyValue = k: v: "${k}=${mkValueString v}";
+  };
+in {
+  home.packages = [self.pkgs.quand];
+
+  xdg.configFile."quand/config".text = toKeyValue {
+    header = false;
+    mondayfirst = true;
+    past = 0;
+    future = 1;
+    yesterday = "\\033[36myesterday  ";
+    today = "\\033[35;1mtoday      ";
+    tomorrow = "\\033[32mtomorrow   ";
+    special = "\\033[33;3m*special* ";
+  };
 }
