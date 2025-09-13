ea1ecff29722b01049323a366f28ebd2cac2be29
Ratakor <ratakor@disroot.org>
2025-09-13 02:55:22 +0200

    Config niri

--- modules/home/programs/niri/default.nix
+++ modules/home/programs/niri/default.nix
@@ -8,16 +8,20 @@ let
   inherit (lib.modules) mkIf;
 in
 {
-  # so this is quite ugly but:
-  # - niri is not yet available in home-manager (I know about niri-flake but
-  #   it's different from the above WMs config)
-  # - niri has hot reloading so it's better if the config is mutable
-  # - tbh it wouldn't be ugly if home-manager's `file` could make out of store
-  #   symlinks with relative paths
-  #
-  # https://github.com/nix-community/home-manager/issues/2085#issuecomment-2022239332
-  # https://foodogsquared.one/posts/2023-03-24-managing-mutable-files-in-nixos/
   config = mkIf (config.self.displayServer == "wayland") {
+    # btw this is NixOS's programs not home-manager's programs.
+    # guess why we're using this one
+    programs.niri.enable = true;
+
+    # so this is quite ugly but:
+    # - niri is not yet available in home-manager (I know about niri-flake but
+    #   it's different from the above WMs config)
+    # - niri has hot reloading so it's better if the config is mutable
+    # - tbh it wouldn't be ugly if home-manager's `file` could make out of store
+    #   symlinks with relative paths
+    #
+    # https://github.com/nix-community/home-manager/issues/2085#issuecomment-2022239332
+    # https://foodogsquared.one/posts/2023-03-24-managing-mutable-files-in-nixos/
     hm.xdg.configFile."niri/config.kdl".source =
       config.hm.lib.file.mkOutOfStoreSymlink "${config.user.home}/nixos/modules/home/programs/niri/niri-config.kdl";
   };
--- modules/options/colors/module.nix
+++ modules/options/colors/module.nix
@@ -1,6 +1,10 @@
 # This is kind of barebones atm maybe set everything into a single namespace and
 # improve type system for colors. Also move specific programs theme to their own options?
 # The comment above is maybe lying.
+# Maybe revert this into a global module without options
+# Also the naming in wrong for bright, it should be bright.<color> instead.
+# TODO: Add colors' which is colors but format #rrggbb
+# colors' = builtins.mapAttrs (_: c: "#${c}") colors;
 {
   config,
   lib,
