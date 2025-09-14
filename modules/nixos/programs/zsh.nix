7acbc7bb7c889fa47d94d7e4ddb1c88c505fa1fa
Ratakor <ratakor@disroot.org>
2025-09-14 14:53:32 +0200

    Fix zsh completions

--- modules/nixos/programs/zsh.nix
+++ modules/nixos/programs/zsh.nix
@@ -6,14 +6,10 @@
       export ZDOTDIR=$HOME/.config/zsh
     '';
 
-    # From NotAShelf:
-    # We actually like zsh completion, but its setup belongs in home-manager
-    # configuration to better support standalone home-manager installations. We
-    # disable completion here to save time by avoiding duplicate compinit calls
-    # and other similar duplications. Despite being disabled here, zsh will be
-    # able to complete for my home-manager user.
-    # See:
-    #  <https://github.com/nix-community/home-manager/issues/3965>
-    enableCompletion = false;
+    # I think that this controls wether zsh completion output are installed to
+    # the system so it must be set to true even if configuring zsh through
+    # home-manager. Set `no_global_rcs` to prevent duplicate compinit calls.
+    # https://github.com/nix-community/home-manager/issues/3965
+    enableCompletion = true;
   };
 }
