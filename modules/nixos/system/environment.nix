198f24ab30fcb405a681a402d5c9b441a7683c4b
Ratakor <ratakor@disroot.org>
2025-10-22 19:19:53 +0200

    memoryAllocator: warn about not using "libc"

--- modules/nixos/system/environment.nix
+++ modules/nixos/system/environment.nix
@@ -5,12 +5,14 @@
       # MANPAGER = "nvim -c 'set ft=man bt=nowrite noswapfile nobk shada=\\\"NONE\\\" ro noma' +Man! -o -";
     };
 
-    # binsh = "${pkgs.dash}/bin/dash";
     localBinInPath = true;
     homeBinInPath = false;
-    # memoryAllocator.provider = "graphene-hardened";
     enableAllTerminfo = false;
     enableDebugInfo = false; # see wiki to enable debug info per package instead
     extraOutputsToInstall = [ ]; # enable it per package instead like `pkg.dev`
+
+    # Both of these options can be set for performance concerns but your system will break.
+    # binsh = "${pkgs.dash}/bin/dash";
+    # memoryAllocator.provider = "mimalloc"; # "graphene-hardened";
   };
 }
