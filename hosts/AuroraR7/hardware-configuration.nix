66927d132ad977107b5d23b2904be7cb4e523e12
Ratakor <ratakor@disroot.org>
2026-03-30 23:22:43 +0200

    hosts/AuroraR7: add niri nvidia fix to /etc

--- hosts/AuroraR7/hardware-configuration.nix
+++ hosts/AuroraR7/hardware-configuration.nix
@@ -104,4 +104,30 @@
     nvidiaSettings = true;
     # package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
+  # https://niri-wm.github.io/niri/Nvidia.html
+  environment.etc."nvidia/nvidia-application-profiles-rc.d/50-limit-free-buffer-pool-in-wayland-compositors.json".text =
+    ''
+      {
+        "rules": [
+          {
+            "pattern": {
+              "feature": "procname",
+              "matches": "niri"
+            },
+            "profile": "Limit Free Buffer Pool On Wayland Compositors"
+          }
+        ],
+        "profiles": [
+          {
+            "name": "Limit Free Buffer Pool On Wayland Compositors",
+            "settings": [
+              {
+                "key": "GLVidHeapReuseRatio",
+                "value": 0
+              }
+            ]
+          }
+        ]
+      }
+    '';
 }
