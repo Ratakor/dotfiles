3fba8440c1bee73dc26bcb703ca4ee47d51f8589
Ratakor <ratakor@disroot.org>
2026-03-31 15:15:06 +0200

    television: add nix-search-tv

--- modules/home/programs/television/default.nix
+++ modules/home/programs/television/default.nix
@@ -1,9 +1,23 @@
 {
-  hm.programs.television = {
-    enable = true;
-    # Add <C-T> for smart autocomplete & <C-R> for history search
-    enableZshIntegration = true;
-    channels = { }; # TODO
-    settings = { }; # TODO
+  hm.programs = {
+    television = {
+      enable = true;
+      # Add <C-T> for smart autocomplete & <C-R> for history search
+      enableZshIntegration = true;
+      channels = { };
+      settings = { };
+    };
+    nix-search-tv = {
+      enable = true;
+      settings = {
+        indexes = [
+          "nixpkgs"
+          "nixos"
+          "home-manager"
+          "noogle"
+        ];
+        update_interval = "24h";
+      };
+    };
   };
 }
