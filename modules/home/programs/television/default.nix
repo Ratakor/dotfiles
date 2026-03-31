85aabc774ecaf77358355c13d2f17b3d95de6e61
Ratakor <ratakor@disroot.org>
2026-03-31 14:52:26 +0200

    television: init program

---
+++ modules/home/programs/television/default.nix
@@ -0,0 +1,9 @@
+{
+  hm.programs.television = {
+    enable = true;
+    # Add <C-T> for smart autocomplete & <C-R> for history search
+    enableZshIntegration = true;
+    channels = { }; # TODO
+    settings = { }; # TODO
+  };
+}
