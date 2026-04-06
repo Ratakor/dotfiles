8e701433a4ef70d4856687973d9c8873f1da81e9
Ratakor <ratakor@disroot.org>
2026-04-06 17:09:30 +0200

    nushell: add more settings

--- modules/home/programs/nushell/default.nix
+++ modules/home/programs/nushell/default.nix
@@ -5,14 +5,31 @@
     nushell = {
       enable = false;
       # home-manager issue ig
-      environmentVariables = config.hm.home.sessionVariables;
+      environmentVariables = config.hm.home.sessionVariables // {
+        # prompt indicator handled by oh-my-posh
+        PROMPT_INDICATOR = "";
+        PROMPT_INDICATOR_VI_NORMAL = "";
+        PROMPT_INDICATOR_VI_INSERT = "";
+        PROMPT_MULTILINE_INDICATOR = "> ";
+      };
+      # see `config nu --doc`
       settings = {
-        show_banner = false;
+        history = {
+          file_format = "sqlite";
+          # Basically disable sharing command history between nu sessions
+          sync_on_enter = false; # plaintext only
+          isolation = true; # sqlite only
+        };
+        show_banner = false; # "short" for startup time
+        # rm.always_trash = true; # TODO: depends if filesystem snapshots are enabled
         edit_mode = "vi";
-        # TODO: setup the rest
+        cursor_shape = {
+          vi_insert = "line";
+          vi_normal = "block";
+        };
       };
       plugins = [
-        # All of belows are available in nixpkgs
+        # TODO: All of belows are available in nixpkgs
         # https://github.com/FMotalleb/nu_plugin_desktop_notifications
         # https://github.com/JosephTLyons/nu_plugin_units
         # https://github.com/idanarye/nu_plugin_skim
