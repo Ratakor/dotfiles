777e81185eea440443f94af9db607f874c666bc9
Ratakor <ratakor@disroot.org>
2026-04-08 23:49:24 +0200

    modules/nixos: unlock keyring via greetd

--- modules/nixos/security/pam.nix
+++ modules/nixos/security/pam.nix
@@ -5,6 +5,17 @@
       swaylock = {
         fprintAuth = false;
       };
+      # Unlock gnome-keyring and GPG on login via greetd.
+      # Replace greetd with login if using another display manager.
+      # https://wiki.nixos.org/wiki/Secret_Service#Auto-decrypt_on_login
+      greetd = {
+        enableGnomeKeyring = true;
+        # gnupg = {
+        #   enable = true;
+        #   noAutoStart = true;
+        #   storeOnly = true;
+        # };
+      };
     };
   };
 }
