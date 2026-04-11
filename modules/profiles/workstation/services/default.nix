a3bb4d6ce907717b84aceea936071658c74dbdf8
Ratakor <ratakor@disroot.org>
2026-04-11 04:32:20 +0200

    profiles/workstation: reduce systemd timeout

--- modules/profiles/workstation/services/default.nix
+++ modules/profiles/workstation/services/default.nix
@@ -4,4 +4,12 @@
   ];
 
   services.dbus.implementation = "broker";
+
+  systemd.settings.Manager = {
+    # Timeout 90s -> 10s to avoid hanging the system on boot or shutdown
+    DefaultTimeoutStartSec = "10s";
+    DefaultTimeoutStopSec = "10s";
+    DefaultTimeoutAbortSec = "10s";
+    DefaultDeviceTimeoutSec = "10s";
+  };
 }
