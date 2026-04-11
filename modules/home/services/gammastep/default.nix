8201d138ca35df9e57a2c76f8b08ea7cf0e50b29
Ratakor <ratakor@disroot.org>
2026-04-11 04:49:39 +0200

    gammastep: inherit config.location

--- modules/home/services/gammastep/default.nix
+++ modules/home/services/gammastep/default.nix
@@ -1,15 +1,13 @@
 # Screen color temperature adjuster -- No blue light at night
 # Check out `sunsetr` for a pretty cool pure Wayland alternative
+{ config, ... }:
 {
   hm.services.gammastep = {
     enable = true;
     settings.general.fade = 0;
     temperature.day = 6000;
     temperature.night = 3000;
-    provider = "geoclue2";
-    # provider = "manual";
-    # latitude = 48.8;
-    # longitude = 2.3;
+    inherit (config.location) provider latitude longitude;
   };
 
   services.geoclue2.appConfig.gammastep = {
