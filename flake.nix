31cd2acb4092d11a3c08c46239b6c5f9d6c6bb83
Ratakor <ratakor@disroot.org>
2026-04-22 15:06:36 +0200

    flake.nix: remove flakes/nix-command from nixConfig

--- flake.nix
+++ flake.nix
@@ -1,16 +1,10 @@
 {
   description = "Ratakor's basic NixOS configuration";
 
-  # Nix configuration to use for this flake.
-  # This doesn't affect the system configuration.
   nixConfig = {
     extra-substituters = [ "https://ratakor.cachix.org" ];
     extra-trusted-public-keys = [ "ratakor.cachix.org-1:9hOGzHtnKDJ1i9FQN87XFnOOpRBebSKWECswk17glP0=" ];
-    extra-experimental-features = [
-      "flakes"
-      "nix-command"
-      "pipe-operators"
-    ];
+    extra-experimental-features = [ "pipe-operators" ];
   };
 
   outputs = inputs: import ./. inputs;
