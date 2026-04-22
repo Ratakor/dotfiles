f60f62eda23ce6421e649022f43bffab8e5b3c58
Ratakor <ratakor@disroot.org>
2026-04-22 06:12:33 +0200

    nixos: use en_IE.UTF-8 locale

--- modules/nixos/system/locale.nix
+++ modules/nixos/system/locale.nix
@@ -13,17 +13,7 @@
 
   # Select internationalisation properties
   i18n = {
-    defaultLocale = "en_US.UTF-8";
-    extraLocaleSettings = {
-      #   LC_ADDRESS = "fr_FR.UTF-8";
-      #   LC_IDENTIFICATION = "fr_FR.UTF-8";
-      LC_MEASUREMENT = "fr_FR.UTF-8";
-      #   LC_MONETARY = "fr_FR.UTF-8";
-      #   LC_NAME = "fr_FR.UTF-8";
-      #   LC_NUMERIC = "fr_FR.UTF-8";
-      #   LC_PAPER = "fr_FR.UTF-8";
-      #   LC_TELEPHONE = "fr_FR.UTF-8";
-      #   LC_TIME = "fr_FR.UTF-8";
-    };
+    # https://unix.stackexchange.com/questions/62316/why-is-there-no-euro-english-locale
+    defaultLocale = "en_IE.UTF-8";
   };
 }
