9f4e091218613fb0f63a861f33392b0809b0018f
Ratakor <ratakor@disroot.org>
2026-04-23 23:49:20 +0200

    flake: remove description & nixConfig

--- flake.nix
+++ flake.nix
@@ -1,11 +1,3 @@
 {
-  description = "Ratakor's basic NixOS configuration";
-
-  nixConfig = {
-    extra-substituters = [ "https://ratakor.cachix.org" ];
-    extra-trusted-public-keys = [ "ratakor.cachix.org-1:9hOGzHtnKDJ1i9FQN87XFnOOpRBebSKWECswk17glP0=" ];
-    extra-experimental-features = [ "pipe-operators" ];
-  };
-
   outputs = inputs: import ./. inputs;
 }
