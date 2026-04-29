6c3a59fbcdfb7fd0b11a02d470c23a001345387a
Ratakor <ratakor@disroot.org>
2026-04-29 13:31:50 +0200

    nixos/fs: config & doc snapper for btrfs
    Signed-off-by: Ratakor <ratakor@disroot.org>

--- modules/nixos/fs/btrfs.nix
+++ modules/nixos/fs/btrfs.nix
@@ -1,45 +1,62 @@
 { config, lib, ... }:
 let
-  # stolen from https://git.darmstadt.ccc.de/piegames/home-config/-/blob/master/machines/prismarine/snapper.nix
-  # this needs tuning
-  mkSnapperConfig = subvolume: {
-    SUBVOLUME = subvolume; # subvolume to snapshot
-    FSTYPE = "btrfs"; # filesystem type
-    QGROUP = ""; # btrfs qgroup for space aware cleanup algorithms
-    SPACE_LIMIT = "0.5"; # fraction of the filesystems space the snapshots may use
-    FREE_LIMIT = "0.2"; # fraction of the filesystems space that should be free
+  # See snapper-configs(5)
+  mkSnapperConfig = path: {
+    # Path of the subvolume or mount point.
+    SUBVOLUME = path;
+    # Filesystem type for the subvolume.
+    FSTYPE = "btrfs";
+    # The btrfs quoat group used for space aware cleanup algorithms.
+    QGROUP = "";
+    # Threshold for the maximum space snapshots should use on the filesystem.
+    SPACE_LIMIT = "0.5"; # default: 0.5 (50%)
+    # Threshold for the minimum free space that should remain on the filesystem.
+    FREE_LIMIT = "0.2"; # default: 0.2 (20%)
 
-    # users and groups allowed to work with config
-    ALLOW_USERS = [ ];
+    # List of users/groups allowed to operate with the config.
+    ALLOW_USERS = [ config.user.name ];
     ALLOW_GROUPS = [ ];
-    # sync users and groups from ALLOW_USERS and ALLOW_GROUPS to .snapshots directory
+
+    # Whether snapper will sync the users and groups from ALLOW_USERS and
+    # ALLOW_GROUPS to the ACL of the .snapshots directory.
     SYNC_ACL = false;
+    # Whether pre and post snapshots should be compared in the background after creation.
+    BACKGROUND_COMPARISON = true;
 
-    BACKGROUND_COMPARISON = true; # start comparing pre- and post-snapshot in background after creating post-snapshot
+    # Whether the number cleanup algorithm should be run for the config.
+    NUMBER_CLEANUP = true;
+    # Minimal age for snapshots to be deleted by the number cleanup algorithm.
+    NUMBER_MIN_AGE = 3600; # default: 3600
+    # Defines how many snapshots the number cleanup algorithm should keep.
+    NUMBER_LIMIT = 25; # default: 50
+    # Defines how many important snapshots the number cleanup algorithm should keep.
+    NUMBER_LIMIT_IMPORTANT = 10; # default: 10
 
-    # Number cleanup
-    NUMBER_CLEANUP = true; # enable
-    NUMBER_MIN_AGE = 1800;
-    NUMBER_LIMIT = 10;
-    NUMBER_LIMIT_IMPORTANT = 5;
+    # Whether hourly snapshots should be created.
+    TIMELINE_CREATE = true;
+    # Whether the timeline cleanup algorithm should be run for the config.
+    TIMELINE_CLEANUP = true;
+    # Minimal age for snapshots to be deleted by the timeline cleanup algorithm.
+    TIMELINE_MIN_AGE = 3600; # default: 3600
+    # Defines how many hourly snapshots the timeline cleanup algorithm should keep.
+    TIMELINE_LIMIT_HOURLY = 24; # default: 10
+    # Defines how many daily snapshots the timeline cleanup algorithm should keep.
+    TIMELINE_LIMIT_DAILY = 7; # default: 10
+    # Defines how many weekly snapshots the timeline cleanup algorithm should keep.
+    TIMELINE_LIMIT_WEEKLY = 4; # default: 0
+    # Defines how many monthly snapshots the timeline cleanup algorithm should keep.
+    TIMELINE_LIMIT_MONTHLY = 12; # default: 10
+    # Defines how many yearly snapshots the timeline cleanup algorithm should keep.
+    TIMELINE_LIMIT_YEARLY = 0; # default: 10
 
-    TIMELINE_CREATE = true; # Create hourly snapshots
-    TIMELINE_CLEANUP = true; # Cleanup hourly snapshots after some time
-
-    # limits for timeline cleanup
-    TIMELINE_MIN_AGE = 1800;
-    TIMELINE_LIMIT_HOURLY = 5;
-    TIMELINE_LIMIT_DAILY = 1;
-    TIMELINE_LIMIT_WEEKLY = 0;
-    TIMELINE_LIMIT_MONTHLY = 0;
-    TIMELINE_LIMIT_YEARLY = 0;
-
-    EMPTY_PRE_POST_CLEANUP = true; # cleanup empty pre-post-pairs
-    EMPTY_PRE_POST_MIN_AGE = 1800; # limits for empty pre-post-pair cleanup
+    # Defines whether the empty-pre-post cleanup algorithm should be run for the config.
+    EMPTY_PRE_POST_CLEANUP = true;
+    # Minimal age for snapshots to be deleted by the empty-pre-post cleanup algorithm.
+    EMPTY_PRE_POST_MIN_AGE = 3600; # default: 3600
   };
 in
 {
-  config = lib.mkIf config.boot.supportedFilesystems.btrfs {
+  config = lib.mkIf (config.boot.supportedFilesystems.btrfs or false) {
     services = {
       btrfs.autoScrub = {
         enable = true;
