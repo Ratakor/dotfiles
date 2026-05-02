16601c441d433c37458d33e4994d590cb6ad164e
Ratakor <ratakor@disroot.org>
2026-05-02 03:42:03 +0200

    hosts/AuroraR7: fix monitors config
    Signed-off-by: Ratakor <ratakor@disroot.org>

--- hosts/AuroraR7/self.nix
+++ hosts/AuroraR7/self.nix
@@ -8,14 +8,14 @@
           name = "Microstep MSI MPG27CQ2 0x30304E37";
           width = 2560;
           height = 1440;
-          refreshRate = "119.998"; # 143.999 is broken
-          variableRefreshRate = false; # TODO: try to enable this
+          # refreshRate = "119.998"; # 143.999 is broken
+          variableRefreshRate = true;
           scale = 1.3;
         }
         {
           name = "Ancor Communications Inc VX238 GCLMRS016906";
           width = 1920;
-          height = 1440;
+          height = 1080;
           refreshRate = "60.000";
           x = 1970;
           y = 130;
