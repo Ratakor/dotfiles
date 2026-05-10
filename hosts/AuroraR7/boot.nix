31ff79fccd1d4c6ed0b542dec1a26ca5ab44e798
Ratakor <ratakor@disroot.org>
2026-05-10 18:58:28 +0200

    hosts/AuroraR7: silence zfs forceImportRoot warning
    Signed-off-by: Ratakor <ratakor@disroot.org>

--- hosts/AuroraR7/boot.nix
+++ hosts/AuroraR7/boot.nix
@@ -8,10 +8,14 @@
       "usb_storage"
       "sd_mod"
     ];
+
     # # I used to `modprobe dell-smm-hwmon ignore_dmi=1`
     # # but it seems to work (`sensors`) without it so idk
     # extraModeprobeConfig = ''
     #   options dell-smm-hwmon ignore_dmi=1
     # '';
+
+    # Silence a warning introduced in 26.11
+    zfs.forceImportRoot = false;
   };
 }
