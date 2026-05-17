440faf629aee0d71272b45f60820651e931a282e
Ratakor <ratakor@disroot.org>
2026-05-17 16:52:51 +0200

    hosts/AuroraR7: fix suspend
    Signed-off-by: Ratakor <ratakor@disroot.org>

--- hosts/AuroraR7/nvidia.nix
+++ hosts/AuroraR7/nvidia.nix
@@ -1,12 +1,6 @@
-{ config, ... }:
 {
   hardware.nvidia = {
-    package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
+    branch = "legacy_580";
     open = false;
-    powerManagement.enable = false; # not supported
-  };
-  systemd.sleep.settings.Sleep = {
-    AllowSuspend = "no";
-    AllowHibernation = "no";
   };
 }
