533435360aa28dee89d67605abb7a5a7b6eac9b9
Ratakor <ratakor@disroot.org>
2026-05-25 23:33:12 +0200

    nixos/bluetooth: disable blueman
    Signed-off-by: Ratakor <ratakor@disroot.org>

--- modules/nixos/hardware/bluetooth.nix
+++ modules/nixos/hardware/bluetooth.nix
@@ -19,6 +19,6 @@ in
 
     # https://wiki.nixos.org/wiki/Bluetooth
     # Pair bluetooth devices without GUI via blueman-applet and blueman-manager
-    services.blueman.enable = true;
+    services.blueman.enable = false; # using dms
   };
 }
