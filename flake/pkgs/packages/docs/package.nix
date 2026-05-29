78137632a27c7389c7dab0b3ce47c6d5f276d1dc
Ratakor <ratakor@disroot.org>
2026-05-29 09:30:42 +0200

    packages/docs: fix "declared in"
    Signed-off-by: Ratakor <ratakor@disroot.org>

--- flake/pkgs/packages/docs/package.nix
+++ flake/pkgs/packages/docs/package.nix
@@ -6,24 +6,23 @@
   ...
 }:
 let
-  inherit (lib.filesystem) listModuleFiles;
-  inherit (lib.lists) singleton;
-
   # This depends on `ndg` being in `pkgs`.
   ndg-builder = pkgs.callPackage "${sources.ndg}/nix/packages/ndg-builder/package.nix" { };
 
   eval = import "${sources.nixpkgs}/nixos/lib/eval-config.nix" {
     inherit lib pkgs;
     system = null;
-    modules = listModuleFiles (self + /modules/options);
+    modules = lib.listModuleFiles (self + /modules/options);
     specialArgs = {
       inherit sources;
+      inherit (self) keys;
     };
   };
 in
 ndg-builder.override {
-  rawModules = singleton { options.self = eval.options.self; };
-  moduleName = "self"; # idk
+  evaluatedModules.options.self = eval.options.self;
+  moduleName = "self";
+  basePath = self;
   repoPath = "https://github.com/ratakor/dotfiles/blob/nixos";
   title = "Self";
   description = "Options available via config.self";
