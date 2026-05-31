970b19c5e30cf6f706919d1160cb5c53538e25a3
Ratakor <ratakor@disroot.org>
2026-05-31 20:01:50 +0200

    kde: remove qtct
    Signed-off-by: Ratakor <ratakor@disroot.org>

--- modules/nixos/programs/xdg/portal/kde/default.nix
+++ modules/nixos/programs/xdg/portal/kde/default.nix
@@ -28,34 +28,15 @@ in
     services.dbus.packages = dolphin;
     user.packages = dolphin;
 
-    hm.qt =
-      let
-        qtctSettings = {
-          Appearance = {
-            custom_palette = false;
-            style = "kvantum";
-            # icon_theme = "Papirus-Dark";
-            color_scheme = "Style's Color";
-          };
-          # TODO: there should be config.self.style.{colors,fonts} with
-          # monospace, sansSerif, ... in fonts
-          Fonts = {
-            fixed = "\"Agave Nerd Font Mono,12\"";
-            general = "\"Luciole,12\"";
-          };
-        };
-      in
-      {
+    hm.qt = {
+      enable = true;
+      style.name = "kvantum";
+      platformTheme.name = "kde"; # "qtct" doesn't work with dolphin
+      kvantum = {
         enable = true;
-        style.name = "kvantum";
-        platformTheme.name = "kde"; # "qtct" doesn't work with dolphin
-        # qt5ctSettings = qtctSettings;
-        # qt6ctSettings = qtctSettings;
-        kvantum = {
-          enable = true;
-          settings.General.theme = theme.name;
-          themes = singleton theme.package;
-        };
+        settings.General.theme = theme.name;
+        themes = singleton theme.package;
       };
+    };
   };
 }
