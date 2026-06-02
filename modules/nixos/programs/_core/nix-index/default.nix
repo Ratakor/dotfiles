877130c5837c9c2bc1b1bb3622c4d7ebc9a3c0a8
Ratakor <ratakor@disroot.org>
2026-06-02 03:27:58 +0200

    hjem: nix-index-database
    Signed-off-by: Ratakor <ratakor@disroot.org>

--- modules/nixos/programs/_core/nix-index/default.nix
+++ modules/nixos/programs/_core/nix-index/default.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  pkgs,
   sources,
   ...
 }:
@@ -7,21 +8,25 @@ let
   inherit (lib.modules) mkForce;
 in
 {
-  hm.imports = [ "${sources.nix-index-database}/home-manager-module.nix" ];
+  imports = [ "${sources.nix-index-database}/nixos-module.nix" ];
 
-  hm.programs = {
+  programs = {
     # A file database for nixpkgs
     nix-index = {
       enable = true;
-      symlinkToCacheHome = true;
       enableZshIntegration = false; # We use comma instead
-      enableNushellIntegration = true;
+      enableBashIntegration = false; # why is this true by default?
     };
-    # A combination of nix-index and nix run
-    nix-index-database.comma.enable = true;
+
+    nix-index-database = {
+      enable = true;
+      comma.enable = true;
+    };
+
     # afaik this uses nix-channel which we do not support
     command-not-found.enable = mkForce false;
   };
 
-  programs.command-not-found.enable = mkForce false; # :)
+  hj.xdg.cache.files."nix-index/files".source =
+    (import sources.nix-index-database { inherit pkgs; }).nix-index-database;
 }
