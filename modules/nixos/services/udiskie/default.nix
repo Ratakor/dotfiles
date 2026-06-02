12cf252dff367d69d7dd13bfc5261e85637a9540
Ratakor <ratakor@disroot.org>
2026-06-02 03:12:03 +0200

    hjem: udiskie
    Signed-off-by: Ratakor <ratakor@disroot.org>

--- modules/nixos/services/udiskie/default.nix
+++ modules/nixos/services/udiskie/default.nix
@@ -1,23 +1,48 @@
 # USB device manager (auto-mounting)
-{ config, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 let
+  inherit (lib.modules) mkIf;
   dprg = config.self.programs.default;
   cfg = config.self.services.udiskie;
 in
 {
-  config = lib.mkIf cfg.enable {
+  config = mkIf cfg.enable {
     services.udisks2.enable = true;
+    user.packages = [ pkgs.udiskie ];
 
+    systemd.user.services.udiskie = {
+      description = "udiskie auto-mount daemon";
+      bindsTo = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      wantedBy = [ "graphical-session.target" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.udiskie}/bin/udiskie --automount --notify --tray never";
+        Restart = "always";
+      };
+    };
+
+    hj.xdg.config.files."udiskie/config.yml".text = ''
+      program_options:
+        terminal: ${dprg.terminal.cmdDir}
+    '';
+
+    /*
     hm.services.udiskie = {
       enable = true;
       automount = true;
       notify = true;
-      tray = "never"; # gmod style no icon
+      tray = "never";
       settings = {
         program_options = {
           terminal = dprg.terminal.cmdDir;
         };
       };
     };
+    */
   };
 }
