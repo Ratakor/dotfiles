abda2dcadfad0991b9e17ffc93eefffa901eac92
Ratakor <ratakor@disroot.org>
2026-06-03 20:23:08 +0200

    profiles/workstation: enable x11 support for wine
    Signed-off-by: Ratakor <ratakor@disroot.org>

--- modules/profiles/workstation/system/packages.nix
+++ modules/profiles/workstation/system/packages.nix
@@ -6,7 +6,8 @@
     hunspellDicts.en-us
     hunspellDicts.fr-moderne
 
-    # not an emulator btw (also wayland support is experimental)
-    wineWow64Packages.waylandFull
+    # not an emulator btw
+    # wineWow64Packages.waylandFull # unstable (doesn't work)
+    wineWow64Packages.stableFull
   ];
 }
