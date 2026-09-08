496ff2b72ae3939a6551a081d64b6f20cf9a8e98
Ratakor <ratakor@disroot.org>
2026-09-08 17:14:39 +0200

    zed: add some default settings
    Signed-off-by: Ratakor <ratakor@disroot.org>

--- modules/nixos/programs/editor/zed/default.nix
+++ modules/nixos/programs/editor/zed/default.nix
@@ -1,3 +1,4 @@
+# TODO: for EDITOR and VISUAL sessionVariables we should maybe add `-w` flag
 { config, lib, ... }:
 let
   inherit (lib.modules) mkIf;
@@ -15,7 +16,23 @@ in
 
     hm.programs.zed-editor = {
       enable = true;
-      # TODO: config
+      # TODO: config...
+      userSettings = {
+        relative_line_numbers = "enabled";
+        helix_mode = true;
+        cursor_blink = false;
+        session.trust_all_worktrees = true; # idk ig it's alright
+        # TODO
+        theme = {
+          mode = "system";
+          light = "Gruvbox Light";
+          dark = "Gruvbox Dark";
+        };
+        wrap_guides = [
+          80
+          100
+        ];
+      };
     };
 
     # This should be in shell.variables.nix, also here is previous comment:
